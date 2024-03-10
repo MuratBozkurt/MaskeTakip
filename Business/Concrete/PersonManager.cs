57af75b218a01f0f052d49f394a47bdc9664526e
MuratBozkurt <muratbozkurt.2317@hotmail.com>
2024-03-10 21:24:24 +0300

    Add project files.

---
+++ Business/Concrete/PersonManager.cs
@@ -0,0 +1,25 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class PersonManager : IApplicantService
+    {
+        public void ApplyForMask(Person person)
+        {
+
+        }
+        public List<Person> GetList()
+        {
+            return null;
+        }
+        public bool CheckPerson(Person person)
+        {
+            // Mernis servisi kapalıydı
+            return true;
+        }
+    }
+}
---
+++ Workaround/Program.cs
@@ -0,0 +1,49 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System;
+
+
+namespace Workaround
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SelamVer("Murat");
+            
+            string[] ogrenciler = new string[3];
+            ogrenciler[0] = "Murat";
+            ogrenciler[1] = "Orçin";
+            ogrenciler[2] = "Ali";
+
+            ogrenciler = new string[4];
+            ogrenciler[3] = "Ahmet";
+
+            for (int i = 0; i < ogrenciler.Length; i++)
+            {
+                Console.WriteLine(ogrenciler[i]);
+            }
+
+            Person person1 = new Person();
+            person1.DateOfBirthYear = 1983;
+            person1.FirstName = "ALİ VELİ";
+            person1.LastName = "PAYA";
+            person1.NationalIdentity = 12345678902;
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+            
+            Console.ReadLine();
+
+        }
+
+        static void SelamVer(string isim = "isimsiz")
+        {
+            Console.WriteLine("Merhaba " + isim);
+        }
+
+
+    }
+
+
+}
